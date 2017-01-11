cf2563a75c704e5216e0e96e8624dd0d856252bd
Uday Kiran Ailapaka <e003620@Admins-MacBook-Pro-2.local>
2017-01-11 20:16:32 +0530

    Home, Subscriptions implemented

--- Amaysim/ViewControllers/ASSubscriptionsInfoViewController.swift
+++ Amaysim/ViewControllers/ASSubscriptionsInfoViewController.swift
@@ -8,24 +8,41 @@
 
 import UIKit
 
-class ASSubscriptionsInfoViewController: UIViewController {
+class ASSubscriptionsInfoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableVew: UITableView!
 
+    var services: [Any] = [];
+    var subscriptions: [Any] = []
+    var products: [Any] = []
+    
     let helper = ASHelper.sharedInstance;
-    var attributes : [[String : AnyObject]] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableVew.separatorStyle = UITableViewCellSeparatorStyle.none
         self.tableVew.register(UINib.init(nibName: "ProfileCell", bundle: nil), forCellReuseIdentifier: "ProfileCell")
-        self.tableVew.rowHeight = 54;
-        if let attributes = helper.profile?["attributes"] as? [String : AnyObject]{
-            self.attributes.append(["contact-number":attributes["contact-number"]!]) ;
-            self.attributes.append(["email-address":attributes["email-address"]!]) ;
-            self.attributes.append(["date-of-birth":attributes["date-of-birth"]!]) ;
-            self.attributes.append(["payment-type":attributes["payment-type"]!]) ;
-            
+        self.tableVew.rowHeight = 44;
+        if let services = helper.services?["attributes"] as? [String: AnyObject] {
+            for (n, c) in services.enumerated() {
+                print("\(n): '\(c)'")
+                self.services.append(c as Any);
+            }
         }
+        if let subscriptions = helper.subscriptions?["attributes"] as? [String: AnyObject] {
+            for (n, c) in subscriptions.enumerated() {
+                print("\(n): '\(c)'")
+                self.subscriptions.append(c as Any);
+            }
+        }
+        if let products = helper.products?["attributes"] as? [String: AnyObject] {
+            for (n, c) in products.enumerated() {
+                print("\(n): '\(c)'")
+                self.products.append(c as Any);
+            }
+        }
+//        self.services = helper.services?["attributes"] as? [String: AnyObject]
+//        self.subscriptions = helper.subscriptions?["attributes"]  as? [String : AnyObject]
+//        self.products = helper.products?["attributes"]  as? [String : AnyObject]
     }
     
     @IBAction func closeView(_ sender: Any) {
@@ -34,17 +51,58 @@ class ASSubscriptionsInfoViewController: UIViewController {
     
     //    MARK: UITableView Delegates and DataSources
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1;
+        return 3;
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.attributes.count;
+        
+        switch section {
+        case 0:
+            return self.services.count ;
+        case 1:
+            return self.subscriptions.count;
+        case 2:
+            return self.products.count;
+        default:
+            return 0;
+        }
+
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Services";
+        case 1:
+            return "Subscription Details";
+        case 2:
+            return "Product Details";
+        default:
+            return "";
+        }
+    }
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell")! as UITableViewCell
-        let dict : [String:AnyObject] = self.attributes[indexPath.row]
-        cell.textLabel?.text  = dict.keys.first
-        cell.detailTextLabel?.text  = dict.values.first as? String
+        var dict : (String,AnyObject)?
+        switch indexPath.section {
+        case 0:
+            dict  = self.services[indexPath.row] as? (String,AnyObject)
+        case 1:
+            dict  = self.subscriptions[indexPath.row] as? (String,AnyObject)
+        case 2:
+            dict  = self.products[indexPath.row] as? (String,AnyObject)
+        default: break
+        }
+        cell.textLabel?.text  = dict?.0
+        cell.detailTextLabel?.text  = "---"
+        if let val =  dict?.1 as? String{
+             print(val);
+            cell.detailTextLabel?.text  = val
+        }
+        if let val =  dict?.1 as? NSNumber{
+            print(val);
+            cell.detailTextLabel?.text  = String(describing: val)
+        }
         return cell;
     }
 }
